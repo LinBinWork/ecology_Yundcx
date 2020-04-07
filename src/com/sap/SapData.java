c9ac185f2f4aea2063056575cb7dfff7a0ef36f0
linbin <linbin_work@foxmail.com>
2020-04-07 11:25:41 +0800

    导入 sap

---
+++ src/com/sap/SapData.java
@@ -0,0 +1,60 @@
+package com.sap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class SapData {
+
+    // 列数
+    private int fieldCount;
+
+    // 列的具体名称
+    private String[] fieldNames;
+
+    // 全部数据
+    private List<Map<String, String>> data;
+
+    public SapData(int fieldCount, String[] fieldNames, List<Map<String, String>> data) {
+        this.fieldCount = fieldCount;
+        this.fieldNames = fieldNames;
+        this.data = data;
+    }
+
+    public SapData() {
+
+    }
+
+    public int getFieldCount() {
+        return fieldCount;
+    }
+
+    public void setFieldCount(int fieldCount) {
+        this.fieldCount = fieldCount;
+    }
+
+    public String[] getFieldNames() {
+        return fieldNames;
+    }
+
+    public void setFieldNames(String[] fieldNames) {
+        this.fieldNames = fieldNames;
+    }
+
+    public List<Map<String, String>> getData() {
+        return data;
+    }
+
+    public void setData(List<Map<String, String>> data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "SapData{" +
+                "fieldCount=" + fieldCount +
+                ", fieldNames=" + Arrays.toString(fieldNames) +
+                ", data=" + data +
+                '}';
+    }
+}
